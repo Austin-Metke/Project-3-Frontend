c090f78eeb961275d35f293316cc3af64294f245
Donaldson10 <166642140+Donaldson10@users.noreply.github.com>
2025-10-29 23:32:24 -0700

    Converted to Expo
    Converted to expo app

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: 'globalThis',
+  },
+  resolve: {
+    alias: {
+      'react-native': 'react-native-web',
+    },
+  },
+  optimizeDeps: {
+    include: ['react-native-web'],
+  },
 })
